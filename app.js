bff21fa4ca3c108ed917733395332c30933074f6
chris.chiang <jieyu0702@gmail.com>
2018-08-03 17:56:39 +0800

    rest api save remove sales car

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 
 var webRouter = require("./lib/web_router");
-var userRestApi = require("./lib/rest_api/user");
-var fileRestApi = require("./lib/rest_api/file");
+
 var carRestApi = require("./lib/rest_api/car");
-var chatSocket = require("./lib/chat_socket");
+var salesRestApi = require("./lib/rest_api/sales");
+
+
+
+
 var dataBaseInit = require("./lib/db/dataBaseInit");
 var exeRestApi = require("./lib/rest_api/exe");
 var config = require('./lib/config').config;
@@ -19,11 +22,10 @@ let path = require('path');
 dataBaseInit.mongoDBInit(mongoDBPort,mongoDBName);
 serverUse.on(app);
 webRouter.on(app);
-userRestApi.on(app);
-fileRestApi.on(app);
-exeRestApi.on(app);
+
 carRestApi.on(app);
-chatSocket.on(app,server);
+salesRestApi.on(app);
+
 server.listen(process.env.PORT||serverPort);
 console.log("iis 現在使用" + process.env.PORT + " port");
 console.log("現在使用" + serverPort + "port");
