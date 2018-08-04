d286e04b6f19eb7bcaea44fbd862fd794a9ea5aa
chris.chiang <jieyu0702@gmail.com>
2018-08-04 15:09:41 +0800

    rest api getCompanyList

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
-
 var webRouter = require("./lib/web_router");
-
 var carRestApi = require("./lib/rest_api/car");
 var salesRestApi = require("./lib/rest_api/sales");
 var staffRestApi = require("./lib/rest_api/staff");
 var testDriveRestApi = require("./lib/rest_api/testDrive");
+var companyRestApi = require("./lib/rest_api/company");
 var dataBaseInit = require("./lib/db/dataBaseInit");
-var exeRestApi = require("./lib/rest_api/exe");
 var config = require('./lib/config').config;
 var serverPort = config.serverPort;
 var mongoDBPort = config.mongoDBPort;
@@ -25,6 +23,7 @@ carRestApi.on(app);
 salesRestApi.on(app);
 staffRestApi.on(app);
 testDriveRestApi.on(app);
+companyRestApi.on(app);
 
 server.listen(process.env.PORT||serverPort);
 console.log("iis 現在使用" + process.env.PORT + " port");
