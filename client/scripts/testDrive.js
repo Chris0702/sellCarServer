71214552e01c145377749800586e9d2314e5b085
Christopher.Chiang <Christopher.Chiang@advantech.com>
2018-09-04 15:28:35 +0800

    testdrive init

--- client/scripts/testDrive.js
+++ client/scripts/testDrive.js
@@ -17,27 +17,15 @@ function getTestDriveAll() {
             console.log('success')
             result = JSON.parse(result);
             console.log(result)
+            $('#content').children().remove();
+            // addTestDrive()
             if (result.resStatus == 0) {
-                displayCars(result.resString)
+                displayTestDrive(result.resString)
             }
         }
     });
 }
 
-function displayTestDrive(testDrives) {
-    $('#content').children().remove();
-    // addCar()
-    // if (cars.length > 0) {
-    //     for (var i = 0; i < cars.length; i++) {
-    //         createCar(cars[i])
-    //     }
-    // }
-}
-
-//saveTestDriveInfo(null, name, company, phone,address,payment_type, car_name,car_company ,car_version,car_color, hopeTime, null,
-
-
-function addTestDrive(){
 
 //     address
 // :
@@ -77,6 +65,171 @@ function addTestDrive(){
 // "5b8906af4f379004a088ff89"
 
 
+function displayTestDrive(testDrives) {
+    
+    addTestDrive()
+    if (testDrives.length > 0) {
+        for (var i = 0; i < testDrives.length; i++) {
+            createTestDrive(testDrives[i])
+        }
+    }
+}
+
+//saveTestDriveInfo(null, name, company, phone,address,payment_type, car_name,car_company ,car_version,car_color, hopeTime, null,
+
+function createTestDrive(testDrive){
+    var item = $('<div/>', {
+        class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
+    }).appendTo($('#content'));
+    item.css('width', '100%');
+    item.css('margin-top', '1%');
+     var titleName = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+    titleName.html("姓名: ")
+
+    var name = $('<input/>', {
+        type: 'text',
+        value: testDrive.name,
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+
+     var titlePhone = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+    titlePhone.html("電話: ")
+
+    var phone = $('<input/>', {
+        type: 'text',
+        value: testDrive.phone,
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+
+    var titleCompany = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+    titleCompany.html("公司&機構: ")
+
+    var company = $('<input/>', {
+        type: 'text',
+        value: testDrive.company,
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+
+    var titleCarName = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+    titleCarName.html("車型: ")
+
+    var carName = $('<input/>', {
+        type: 'text',
+        value: testDrive.car_name,
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+
+    var titleColor = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+    titleColor.html("車色: ")
+
+    var color = $('<input/>', {
+        type: 'text',
+        value: testDrive.car_color,
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+
+     var titlePaymentType = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+    titlePaymentType.html("付款方式: ")
+
+    var paymentType = $('<select/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+
+    paymentType.append($("<option></option>").attr("value", "現金").text("現金"));
+    paymentType.append($("<option></option>").attr("value", "刷卡").text("刷卡"));
+
+    // paymentType.append($("<option></option>").attr("value", "刷卡").text("刷卡"));
+
+    var item2 = $('<div/>', {
+        class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
+    }).appendTo($('#content'));
+    item2.css('width', '100%');
+    item2.css('margin-top', '1%');
+
+    var titleAddress = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    titleAddress.html("地址: ")
+
+    var address = $('<input/>', {
+        type: 'text',
+         value: testDrive.address,
+        class: 'col-md-3 col-sm-3 col-xs-3 col-lg-3'
+    }).appendTo(item2);
+
+    // var titleHopeDate = $('<div/>', {
+    //     class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    // }).appendTo(item2);
+    // titleHopeDate.html("試乘日期: ")
+
+    // var hopedate = $('<input/>', {
+    //     type: 'date',
+    //     class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    // }).appendTo(item2);
+
+    var titleHopeTime = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    titleHopeTime.html("試乘時間: ")
+
+    var hopeTime = $('<input/>', {
+        type: 'text',
+        value: testDrive.hopeTime,
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+
+
+    var titleStatus = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    titleStatus.html("狀態: ")
+
+    let orderReceive = 'receive';
+let orderPending = 'pending';
+let orderFailure = 'failure';
+let orderSuccess = 'success';
+
+    var status = $('<select/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+
+    status.append($("<option></option>").attr("value", "receive").text("receive"));
+    status.append($("<option></option>").attr("value", "pending").text("pending"));
+    status.append($("<option></option>").attr("value", "failure").text("failure"));
+    status.append($("<option></option>").attr("value", "success").text("success"));
+
+
+
+    $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+
+    var change = $('<button/>', {
+        class: 'btn btn-success col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    change.html("修改")
+
+    var remove = $('<button/>', {
+        id: testDrive._id,
+        class: 'btn btn-danger col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    remove.html("刪除")
+    // setAddClick(add, name, company, color, price, special_price, description)
+}
+
+function addTestDrive(){
     var item = $('<div/>', {
         class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
     }).appendTo($('#content'));
@@ -92,6 +245,16 @@ function addTestDrive(){
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
 
+     var titlePhone = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+    titlePhone.html("電話: ")
+
+    var phone = $('<input/>', {
+        type: 'text',
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item);
+
     var titleCompany = $('<div/>', {
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
@@ -102,84 +265,12 @@ function addTestDrive(){
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
 
-    var titleColor = $('<div/>', {
+    var titleCarName = $('<div/>', {
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
-    titleColor.html("顏色: ")
+    titleCarName.html("車型: ")
 
-    var color = $('<input/>', {
-        type: 'text',
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleDescription = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleDescription.html("描述: ")
-
-    var description = $('<input/>', {
-        type: 'text',
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleSpecial_price = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleSpecial_price.html("最低價(萬元): ")
-
-    var special_price = $('<input/>', {
-        type: 'text',
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titlePrice = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titlePrice.html("最高價(萬元): ")
-
-    var price = $('<input/>', {
-        type: 'text',
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var item2 = $('<div/>', {
-        class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
-    }).appendTo($('#content'));
-    item2.css('width', '100%');
-    item2.css('margin-top', '1%');
-    var tab = $('<div/>', {
-        class: 'col-md-11 col-sm-11 col-xs-11 col-lg-11'
-    }).appendTo(item2);
-
-    var add = $('<button/>', {
-        class: 'btn btn-primary col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item2);
-    add.html("新增")
-}
-
-
-function addCar(){
-	 var item = $('<div/>', {
-        class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
-    }).appendTo($('#content'));
-    item.css('width', '100%');
-    item.css('margin-top', '1%');
-     var titleName = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleName.html("車型: ")
-
-    var name = $('<input/>', {
-        type: 'text',
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleCompany = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleCompany.html("車廠: ")
-
-    var company = $('<input/>', {
+    var carName = $('<input/>', {
         type: 'text',
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
@@ -187,59 +278,78 @@ function addCar(){
     var titleColor = $('<div/>', {
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
-    titleColor.html("顏色: ")
+    titleColor.html("車色: ")
 
     var color = $('<input/>', {
         type: 'text',
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
 
-    var titleDescription = $('<div/>', {
+     var titlePaymentType = $('<div/>', {
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
-    titleDescription.html("描述: ")
+    titlePaymentType.html("付款方式: ")
 
-    var description = $('<input/>', {
-        type: 'text',
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleSpecial_price = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleSpecial_price.html("最低價(萬元): ")
-
-    var special_price = $('<input/>', {
-        type: 'text',
+    var paymentType = $('<select/>', {
         class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item);
 
-    var titlePrice = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titlePrice.html("最高價(萬元): ")
-
-    var price = $('<input/>', {
-        type: 'text',
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
+    paymentType.append($("<option></option>").attr("value", "現金").text("現金"));
+    paymentType.append($("<option></option>").attr("value", "刷卡").text("刷卡"));
 
     var item2 = $('<div/>', {
         class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
     }).appendTo($('#content'));
     item2.css('width', '100%');
     item2.css('margin-top', '1%');
-    var tab = $('<div/>', {
-        class: 'col-md-11 col-sm-11 col-xs-11 col-lg-11'
+
+    var titleAddress = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    titleAddress.html("地址: ")
+
+    var address = $('<input/>', {
+        type: 'text',
+        class: 'col-md-3 col-sm-3 col-xs-3 col-lg-3'
+    }).appendTo(item2);
+
+    var titleHopeDate = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    titleHopeDate.html("試乘日期: ")
+
+    var hopedate = $('<input/>', {
+        type: 'date',
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+
+    var titleHopeTime = $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    titleHopeTime.html("試乘時段: ")
+
+    var hopetime = $('<select/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
+    }).appendTo(item2);
+    
+    hopetime.append($("<option></option>").attr("value", "早上").text("早上"));
+    hopetime.append($("<option></option>").attr("value", "中午").text("中午"));
+    hopetime.append($("<option></option>").attr("value", "晚上").text("晚上"));
+    hopetime.append($("<option></option>").attr("value", "半夜").text("半夜"));
+
+
+    $('<div/>', {
+        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item2);
 
     var add = $('<button/>', {
         class: 'btn btn-primary col-md-1 col-sm-1 col-xs-1 col-lg-1'
     }).appendTo(item2);
     add.html("新增")
-    setAddClick(add, name, company, color, price, special_price, description)
-
+    // setAddClick(add, name, company, color, price, special_price, description)
 }
+
+
 
 function setAddClick(add, name, company, color, price, special_price, description) {
     add.click(function(e) {
