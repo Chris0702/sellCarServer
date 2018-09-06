a5052921edb5dc0ed0442ef96331953443e8524d
Christopher.Chiang <Christopher.Chiang@advantech.com>
2018-09-06 14:46:52 +0800

    poster ok

--- client/scripts/poster.js
+++ client/scripts/poster.js
@@ -1,5 +1,8 @@
 console.log("js init");
 var selectCompany = "";
+var selectPosters = [];
+var clickColor = "#aaaaaa";
+var normalColor = "#ffffff";
 $(document).ready(function() {
     $('#search_car').click(function(e) {
         console.log('search_car');
@@ -33,15 +36,61 @@ function getPoster() {
     });
 }
 
-function displayPosters(Posters) {
+function displayPosters(posters) {
     $('#content').children().remove();
     addPoster()
     // addCar()
-    // if (cars.length > 0) {
-    //     for (var i = 0; i < cars.length; i++) {
-    //         createCar(cars[i])
-    //     }
-    // }
+    if (posters.length > 0) {
+        for (var i = 0; i < posters.length; i++) {
+            // createCar(cars[i])
+            console.log(i)
+            console.log(posters[i])
+            createImgBlock(posters[i],i)
+        }
+    }
+    imageClickInit()
+}
+
+function createImgBlock(imgSrc, blockIndex) {
+    imgBlockId = 'imgBlock' + blockIndex;
+    var div = $('<div/>', {
+        id: imgBlockId,
+        class: 'thumbnail col-md-2'
+    }).appendTo($('#content'));;
+    // console.log(imgBlockId);
+    // console.log(imgSrc)
+    var img = $('<img />', {
+        id: imgSrc,
+        src: '/' + imgSrc,
+        class: 'posterImage',
+        value: ''
+    }).appendTo($('#' + imgBlockId));
+
+}
+
+function imageClickInit() {
+    $('.posterImage').click(function(e) {
+        var imgId = jQuery(this).attr("id");
+        if (jQuery(this).attr('value') == '') {
+            jQuery(this).attr('value', 'click');
+            jQuery(this).parent().css('background-color', clickColor);
+        } else {
+            jQuery(this).attr('value', '');
+            jQuery(this).parent().css('background-color', normalColor);
+        }
+        var isPush = false;
+        for (var i = 0; i < selectPosters.length; i++) {
+            if (selectPosters[i] == imgId) {
+                selectPosters.splice(i, 1)
+                isPush = true;
+                break;
+            }
+        }
+        if (isPush == false) {
+            selectPosters.push(imgId);
+        }
+        console.log(selectPosters);
+    })
 }
 
 function addPoster() {
@@ -115,133 +164,6 @@ function setAddClick(add, name, company, color, price, special_price, descriptio
     });
 }
 
-function createCar(car) {
-    var item = $('<div/>', {
-        id: car._id,
-        class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
-    }).appendTo($('#content'));
-    item.css('width', '100%');
-    item.css('margin-top', '1%');
-    var titleName = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleName.html("車型: ")
-
-    var name = $('<input/>', {
-        type: 'text',
-        value: car.name,
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleCompany = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleCompany.html("車廠: ")
-
-    var company = $('<input/>', {
-        type: 'text',
-        value: car.company,
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleColor = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleColor.html("顏色: ")
-
-    var color = $('<input/>', {
-        type: 'text',
-        value: car.color,
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleDescription = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleDescription.html("描述: ")
-
-    var description = $('<input/>', {
-        type: 'text',
-        value: car.description,
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titleSpecial_price = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titleSpecial_price.html("最低價(萬元): ")
-
-    var special_price = $('<input/>', {
-        type: 'text',
-        value: car.special_price,
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var titlePrice = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-    titlePrice.html("最高價(萬元): ")
-
-    var price = $('<input/>', {
-        type: 'text',
-        value: car.price,
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item);
-
-    var item2 = $('<div/>', {
-        class: 'col-md-12 col-sm-12 col-xs-12 col-lg-12'
-    }).appendTo($('#content'));
-    item2.css('width', '100%');
-    item2.css('margin-top', '1%');
-    var titleImg = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item2);
-
-    titleImg.html("圖片:")
-
-    var img = $('<img/>', {
-        src: car.imgPath + "?" + Math.random(),
-        class: 'col-md-2 col-sm-2 col-xs-2 col-lg-2'
-    }).appendTo(item2);
-
-    var tab = $('<div/>', {
-        id: car._id,
-        class: 'col-md-2 col-sm-2 col-xs-2 col-lg-2'
-    }).appendTo(item2);
-
-    var titleUploadImg = $('<div/>', {
-        class: 'col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item2);
-    titleUploadImg.html("上傳圖片:")
-    var uploadImg = $('<input/>', {
-        type: 'file',
-        class: 'col-md-3 col-sm-3 col-xs-3 col-lg-3'
-    }).appendTo(item2);
-
-    var upload = $('<button/>', {
-        id: car._id,
-        class: 'btn btn-info col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item2);
-    upload.html("上傳圖片")
-
-    setUploadClick(upload, uploadImg, car.company, car.name)
-
-    var change = $('<button/>', {
-        id: car._id,
-        class: 'btn btn-success col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item2);
-    change.html("修改")
-
-    setChangeClick(change, car._id, name, company, color, price, special_price, description)
-
-    var remove = $('<button/>', {
-        id: car._id,
-        class: 'btn btn-danger col-md-1 col-sm-1 col-xs-1 col-lg-1'
-    }).appendTo(item2);
-    remove.html("刪除")
-    setRemoveClick(remove, car._id)
-}
-
 function uuid() {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
@@ -295,66 +217,30 @@ function setUploadClick(upload, file) {
 
 }
 
-function setRemoveClick(remove, car_id) {
+function setRemoveClick(remove) {
     remove.click(function(e) {
         $.ajax({
-            url: '/car/removeCarInfo',
+            url: '/file/removeFiles',
             type: 'DELETE',
-            headers: {
-                "car_id": car_id
-            },
-            data: {},
-            error: function(xhr) {
-                console.log('error')
-                console.log(xhr)
-            },
-            success: function(result) {
-                console.log('success')
-                console.log(result)
-                result = JSON.parse(result);
-                if (result.resStatus == 0) {
-                    getCars()
-                } else {
-                    alert(result);
-                }
-            }
-        });
-    });
-}
-
-function setChangeClick(change, car_id, name, company, color, price, special_price, description) {
-    change.click(function(e) {
-        var sendColor = color.val();
-        sendColor = sendColor.split(",");
-        sendColor = JSON.stringify(sendColor);
-        $.ajax({
-            url: '/car/saveCarInfo',
-            type: 'POST',
             data: {
-                car_id: car_id,
-                name: name.val(),
-                company: company.val(),
-                price: price.val(),
-                special_price: special_price.val(),
-                description: description.val(),
-                color: sendColor
+                'filesPath':selectPosters
             },
             error: function(xhr) {
                 console.log('error')
                 console.log(xhr)
-                alert(xhr);
             },
             success: function(result) {
                 console.log('success')
                 console.log(result)
                 result = JSON.parse(result);
                 if (result.resStatus == 0) {
-                    getCars()
+                    selectPosters = [];
+                    getPoster()
                 } else {
-                    alert(result);
+                    alert(JSON.stringify(result));
                 }
-
             }
         });
     });
 }
+
